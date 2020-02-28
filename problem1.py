a1d60350a42b3c800bbc1ccb5314b7bdf9a3bac8
Carmen Canedo <carmen.canedo98@gmail.com>
2020-02-28 15:12:56 -0500

    set class node

--- problem1.py
+++ problem1.py
@@ -7,4 +7,14 @@
 #       next
 #  Should have getters and setters
 #
-#  
+
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+    def get_data(self, new_data):
+        return self.data = new_data
+
+    def set_data(self):
+        return self.data
