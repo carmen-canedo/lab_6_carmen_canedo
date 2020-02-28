38b068be86282a4d62d89e9764a767ada386fe77
Carmen Canedo <carmen.canedo98@gmail.com>
2020-02-28 15:00:34 -0500

    wrote out requirements for each problem

--- problem1.py
+++ problem1.py
@@ -1,2 +1,10 @@
 # Lab 6
 # Problem 1
+#
+# Create a node class
+#   Attributes:
+#       data
+#       next
+#  Should have getters and setters
+#
+#  
--- problem2.py
+++ problem2.py
@@ -1,2 +1,13 @@
 # Lab 6
 # Problem 2
+#
+# Create a LinkedList class
+# Attributes:
+#   head
+#   size
+# Create getters and setters
+#
+# Create these methods:
+#   is_empty()
+#   add_node()
+#   remove_node()
